1b58c8cd4df97c5407258861c63c13e74cfa38ee
Brandon <leebran@onid.oregonstate.edu>
2015-08-03 16:01:46 -0700

    Fixed spacing

--- schemas.js
+++ schemas.js
@@ -9,43 +9,43 @@ const Schema = mongoose.Schema;
 
 // Initilize section schema
 const sectionSchema = new Schema({
-  term: String,
-  startDate: Date,
-  endDate: Date,
-  session: String,
-  crn: Number,
-  sec: Number,
-  credits: String,
-  instructor: String,
-  days: String,
-  startTime: Date,
-  endTime: Date,
-  location: String,
-  campus: String,
-  type: String,
-  status: String,
-  enrollCap: Number,
-  enrolled: Number,
-  waitlistCap: Number,
-  waitlisted: Number,
-  fees: String,
-  restrictions: String,
-  comments: String
+    term: String,
+    startDate: Date,
+    endDate: Date,
+    session: String,
+    crn: Number,
+    sec: Number,
+    credits: String,
+    instructor: String,
+    days: String,
+    startTime: Date,
+    endTime: Date,
+    location: String,
+    campus: String,
+    type: String,
+    status: String,
+    enrollCap: Number,
+    enrolled: Number,
+    waitlistCap: Number,
+    waitlisted: Number,
+    fees: String,
+    restrictions: String,
+    comments: String
 });
 
 // -----------------------------------------------------------------------------
 // Initilize course schema
 const courseSchema = new Schema({
-  title: String,
-  abbr: String,
-  credits: String,
-  desc: String,
-  sections: [sectionSchema],
-  updated: { type: Date, default: Date.now },
-  meta: {
+    title: String,
+    abbr: String,
+    credits: String,
+    desc: String,
+    sections: [sectionSchema],
+    updated: { type: Date, default: Date.now },
+    meta: {
     likes: Number,
     dislikes: Number
-  }
+    }
 });
 
 // =============================================================================
