5a9e1eb1dc4a44886934bec4427bb6777b061d54
Brandon <leebran@onid.oregonstate.edu>
2015-08-03 15:48:00 -0700

    Updated spacing formatting

--- app.js
+++ app.js
@@ -40,31 +40,31 @@ app.use('/courses', courses);
 
 // Catch 404 status and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500)
-      .json({
-        message: err.message,
-        error: err,
-      });
-  });
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500)
+          .json({
+            message: err.message,
+            error: err,
+          });
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500)
+    res.status(err.status || 500)
     .json({
-      message: err.message,
-      error: {},
+        message: err.message,
+        error: {},
     });
 });
 
