5800094901f47a2401d8a0c98ee83348f3be438c
Brandon <leebran@onid.oregonstate.edu>
2015-08-03 19:33:34 -0700

    Added commments

--- routes/search.js
+++ routes/search.js
@@ -1,16 +1,27 @@
+// search.js
+
+// Specify strict mode for 'secure' code
 'use_strict';
 
+// Import dependencies and initilize global constants
 const express  = require('express');
 const router   = express.Router();
 const _        = require('underscore');
 const mongoose = require('mongoose');
 const schemas  = require('../schemas');
 
+// =============================================================================
+// MIDDLEWARE FOR ALL REQUESTS
 mongoose.createConnection(process.env.MONGO_URL);
+
+// Initilize mongoose constants
 const db = mongoose.connection;
 const Course = schemas.Course;
 
+// Check for connection errors
 db.on('error', console.error.bind(console, 'connection error:'));
+
+// Run DB
 db.once('open', function(callback) {
 
   // GET: Search course titles
@@ -26,4 +37,6 @@ db.once('open', function(callback) {
   });
 });
 
+// =============================================================================
+// Export as router
 module.exports = router;
