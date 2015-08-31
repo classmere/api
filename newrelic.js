dde6bb89267b75ce29a0191ae3581c59a85c6c46
Rutger Farry <rutgerfarry@gmail.com>
2015-08-30 20:26:12 -0700

    Added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['classmere'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'dbba22b6fa37ee9b2a6d56d120dee526ca0dbe5c',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
