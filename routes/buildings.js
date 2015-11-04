e2a7891cb22bee72bac04693c922a9fa7f994a5e
Rutger Farry <rutgerfarry@gmail.com>
2015-11-03 16:22:52 -0800

    used eslint to lint all files

--- routes/buildings.js
+++ routes/buildings.js
@@ -15,23 +15,23 @@ router.get('/', function getAllBuildings(req, res) {
 // GET: a building
 router.get('/:abbr', function getAllBuildings(req, res) {
   Building.filter({
-  	abbr: req.params.abbr,
+    abbr: req.params.abbr,
   })
   .run()
   .then((result) => {
-  	if (result.length === 0) {
+    if (result.length === 0) {
       res.status(404).json({ 'ohsh***': 'building not found' });
     } else {
-    	const b = result[0];
-    	res.json({
-			abbr: b.abbr,
-			name: b.name,
-			address: b.address,
-			buildingNumber: b.buildingNumber,
-			sqft: b.sqft,
-		});
-	}
-  })
+      const b = result[0];
+      res.json({
+        abbr: b.abbr,
+        name: b.name,
+        address: b.address,
+        buildingNumber: b.buildingNumber,
+        sqft: b.sqft,
+      });
+    }
+  });
 });
 
 module.exports = router;
