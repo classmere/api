b8b00ffa4aec882167edef135b95e8db017ea2f5
Rutger Farry <rutgerfarry@gmail.com>
2016-08-01 19:54:05 -0700

    Deleted thinky stuff, now using mongodb-native

---
+++ sperm-whale/database.js
@@ -0,0 +1,44 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/test';
+
+MongoClient.connect(url, function(err, db) {
+  console.log(`Connected to MongoDB @${url}`);
+  const buildings = db.collection('buildings');
+  const courses = db.collection('courses');
+
+  // Courses
+  module.exports.getAllCourses = function(cb) {
+    courses.find({}, (err, r) => returnRes(err, r, cb));
+  };
+
+  module.exports.getCourse = function(subjectCode, courseNumber, cb) {
+    const abbr = `${subjectCode} ${courseNumber}`;
+    courses.findOne(
+      {abbr: abbr}, 
+      (err, r) => returnRes(err, r, cb)
+    );
+  };
+
+  // Buildings
+  module.exports.getAllBuildings = function(cb) {
+    buildings.find({}, (err, r) => returnRes(err, r, cb));
+  };
+
+  module.exports.getBuilding = function(buildingCode, cb) {
+    buildings.findOne(
+      {abbr: buildingCode}, 
+      (err, r) => returnRes(err, r, cb)
+    );
+  };
+
+  function returnRes(err, r, cb) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, r);
+    }
+  }
+});
+
