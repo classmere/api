12e476f3d5c001c200e53c5d18d9984174755664
Rutger Farry <rutgerfarry@gmail.com>
2018-02-13 23:06:38 -0800

    Switch back to mongorestore for loading test data
    This is sad, because now we are now dependent on mongorestore for tests to function. Using the node driver to load mongoexport data was just too troublesome however (which had no dependencies beyond what npm installed)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,28 +1,15 @@
 /* eslint-env jest */
 
-const fs = require('fs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 const request = require('supertest')
-const MongoClient = require('mongodb').MongoClient
 const app = require('../app')
 
 /// Clears the test database and loads sample data for testing, assuming there
 /// is a MongoDB instance running at localhost or MONGO_URL.
-/// If you don't have MongoDB installed, this requirement can be done with docker:
-/// $ docker run --rm -d -p 27017:27017 mongo
 beforeAll(async () => {
   const url = process.env.MONGO_URL || 'mongodb://localhost:27017/test'
-  const client = await MongoClient.connect(url)
-  const db = client.db('test')
-
-  for (const document of ['buildings']) {
-    const jsonFile = fs.readFileSync(`./__tests__/newdump/${document}.json`)
-    const jsonDocuments = JSON.parse(jsonFile)
-                              .map(d => ({...d, _id: d._id.$oid}))
-    await db.collection(document).deleteMany({})
-    await db.collection(document).insertMany(jsonDocuments)
-  }
-
-  await client.close()
+  await exec(`mongorestore --uri=${url} --drop ./__tests__/dump`)
 })
 
 describe('/courses endpoint', () => {
@@ -30,6 +17,7 @@ describe('/courses endpoint', () => {
     const response = await request(app).get('/courses')
     expect(response.type).toBe('application/json')
     expect(response.statusCode).toBe(200)
+    console.log(response.body)
   })
 
   test('/courses returns an Array', async () => {
